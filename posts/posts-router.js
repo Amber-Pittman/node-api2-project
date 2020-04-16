7f6862a982ccf5bf7d4dc62653eb39c9ad617f16
Amber-Pittman <amber@amberpittman.com>
2020-04-15 20:28:02 -0400

    Get request for post IDs working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -20,17 +20,24 @@ router.get("/", (req, res) => {
 
 //GET request to /api/posts/:id
 router.get("/:id", (req, res) => {
-    console.log(req.query)
     db.findById(req.params.id)
         .then(post => {
-            res.status(200).json(post)
+            console.log("post", post)
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
         })
         .catch(error => {
             console.log(error)
-            res.status(404).json({
-                message: "The post with the specified ID does not exist."
+            res.status(500).json({ 
+                error: "The post information could not be retrieved."
             })
         })
+
 })
 
-module.exports = router;
+module.exports = router
